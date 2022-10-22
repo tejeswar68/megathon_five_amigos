70a6d113b545dae05b3c04848874e7fd1f594334
Tejeswara Murthy palwadi <tejeswarmurthypalwadi@gmail.com>
2022-10-23 00:06:58 +0530

    Added Navigation

--- client/navigation.js
+++ client/navigation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import HomeScreen from './HomeScreen'
+import HomeScreen from './screens/HomeScreen'
 
 const Stack = createStackNavigator();
 
