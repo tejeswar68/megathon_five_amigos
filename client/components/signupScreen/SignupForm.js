b233c9ac587f8c4254561571c22459eaca043639
Vikas Kamarapu <vikaskamarapu2001@gmail.com>
2022-10-23 04:44:36 +0530

    signup form completed

--- client/components/signupScreen/SignupForm.js
+++ client/components/signupScreen/SignupForm.js
@@ -1,5 +1,5 @@
 import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
 import Validator from 'email-validator'
@@ -38,34 +38,57 @@ const styles = StyleSheet.create({
         width: '100%',
         justifyContent: 'center',
         marginTop: 30,
-    }
+    },
+    usersContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        marginVertical: 10,
+    },
+    toggleButton: isSelected => ({
+        marginLeft: 30,
+        backgroundColor: isSelected ? 'orange' : '#dfe8e7',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 5,
+        padding: 10,
+    })
+
 })
 
 const SignupForm = ({ navigation }) => {
+    const phoneRegex = RegExp(
+        /(\+91\ )[6-9]{1}[0-9 ]{4}[0-9 ]{4}[0-9]{3}/,
+    );
+
     const SignupFormSchema = Yup.object().shape({
         username: Yup.string().required().min(2, 'A username is required'),
         email: Yup.string().email().required('An email is required'),
         password: Yup.string().required().min(6, 'Your password has to be at least 6 characters'),
+        phone: Yup.string().matches(phoneRegex, "Invalid phone").required("Phone is required")
     })
-    const onSignup = async (email, password, username) => {
+    const onSignup = async (email, password, username, phone) => {
         await createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
             const user = userCredential.user;
-            setDoc(doc(db, "patients", user.email), {
-                patient_uid: user.uid,
+            setDoc(doc(db, "users", user.email), {
+                user_uid: user.uid,
                 username: username,
                 email: user.email,
-                profile_picture: `https://avatars.dicebear.com/api/bottts/${username}.png?background=%235C5857`
+                phone: phone,
+                isDoctor: !isSelected,
+                profile_picture: `https://avatars.dicebear.com/api/${isSelected ? "bottts" : "open-peeps"}/${username}.png?background=%235C5857`
             });
         }).catch((error) => {
             console.log(error.message)
         });
     }
 
+    const [isSelected, setIsSelected] = useState(true);
+
     return (
         <View style={styles.wrapper}>
             <Formik
-                initialValues={{ username: '', email: '', password: '' }} onSubmit={(values) => {
-                    onSignup(values.email, values.password, values.username);
+                initialValues={{ username: '', email: '', password: '', phone: '' }} onSubmit={(values) => {
+                    onSignup(values.email, values.password, values.username, values.phone);
                 }}
                 validationSchema={SignupFormSchema}
                 validateOnMount={true}
@@ -77,12 +100,11 @@ const SignupForm = ({ navigation }) => {
                             borderWidth: 1,
                         }]}>
                             <TextInput
-                                placeholderTextColor='#5d5d5d'
+                                placeholderTextColor='#d1d1d1'
                                 placeholder='Username'
                                 autoCapitalize='none'
                                 autoFocus={true}
                                 textContentType='username'
-                                style={{ color: 'white' }}
                                 onChangeText={handleChange('username')}
                                 onBlur={handleBlur('username')}
                                 value={values.username}
@@ -93,12 +115,11 @@ const SignupForm = ({ navigation }) => {
                             borderWidth: 1,
                         }]}>
                             <TextInput
-                                placeholderTextColor='#5d5d5d'
+                                placeholderTextColor='#d1d1d1'
                                 placeholder='Email'
                                 autoCapitalize='none'
                                 keyboardType='email-address'
                                 textContentType='emailAddress'
-                                style={{ color: 'white' }}
                                 onChangeText={handleChange('email')}
                                 onBlur={handleBlur('email')}
                                 value={values.email}
@@ -109,21 +130,40 @@ const SignupForm = ({ navigation }) => {
                             borderWidth: 1,
                         }]}>
                             <TextInput
-                                placeholderTextColor='#5d5d5d'
+                                placeholderTextColor='#d1d1d1'
                                 placeholder='Password'
                                 autoCapitalize='none'
                                 autoCorrect={false}
                                 secureTextEntry={true}
                                 textContentType='password'
-                                style={{ color: 'white' }}
                                 onChangeText={handleChange('password')}
                                 onBlur={handleBlur('password')}
                                 value={values.password}
                             />
                         </View>
-                        <View>
-                            <Text>Doctor</Text>
-                            <Text>Patient</Text>
+                        <View style={[styles.inputField, {
+                            borderColor: 1 > values.phone.length || values.phone.length >= 6 ? '#ccfcfc' : 'red',
+                            borderWidth: 1,
+                        }]}>
+                            <TextInput
+                                placeholderTextColor='#d1d1d1'
+                                placeholder='Phone Number'
+                                autoCapitalize='none'
+                                keyboardType='phone-pad'
+                                autoCorrect={false}
+                                textContentType='number'
+                                onChangeText={handleChange('phone')}
+                                onBlur={handleBlur('phone')}
+                                value={values.phone}
+                            />
+                        </View>
+                        <View style={styles.usersContainer}>
+                            <TouchableOpacity onPressOut={() => { setIsSelected(!isSelected) }}>
+                                <Text style={styles.toggleButton(isSelected)}>Patient</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPressOut={() => { setIsSelected(!isSelected) }}>
+                                <Text style={styles.toggleButton(!isSelected)}>Doctor</Text>
+                            </TouchableOpacity>
                         </View>
 
                         <TouchableOpacity style={styles.button(isValid)} onPressOut={handleSubmit}>
@@ -131,7 +171,7 @@ const SignupForm = ({ navigation }) => {
                         </TouchableOpacity>
 
                         <View style={styles.loginContainer}>
-                            <Text style={{ color: '#fff' }}>Already have an account?</Text>
+                            <Text>Already have an account?</Text>
                             <TouchableOpacity onPressOut={() => { navigation.push('LoginScreen') }}>
                                 <Text style={{ color: '#6BB0F5' }}>  Log In</Text>
                             </TouchableOpacity>
