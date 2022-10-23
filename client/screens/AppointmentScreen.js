9e46e44ec4d0f41da288526e97ac87a948924158
Tejeswara Murthy palwadi <tejeswarmurthypalwadi@gmail.com>
2022-10-23 13:03:54 +0530

    Added Appointments

--- client/screens/AppointmentScreen.js
+++ client/screens/AppointmentScreen.js
@@ -1,123 +1,110 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-// import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-// export default function AppointmentScreen() {
+export default function AppointmentScreen() {
 
-//   const [datePicker, setDatePicker] = useState(false);
+  const [datePicker, setDatePicker] = useState(false);
 
-//   const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
 
-//   const [timePicker, setTimePicker] = useState(false);
+  const [timePicker, setTimePicker] = useState(false);
 
-//   const [time, setTime] = useState(new Date(Date.now()));
+  const [time, setTime] = useState(new Date(Date.now()));
 
-//   function showDatePicker() {
-//     setDatePicker(true);
-//   };
+  function showDatePicker() {
+    setDatePicker(true);
+  };
 
-//   function showTimePicker() {
-//     setTimePicker(true);
-//   };
+  function showTimePicker() {
+    setTimePicker(true);
+  };
 
-//   function onDateSelected(event, value) {
-//     setDate(value);
-//     setDatePicker(false);
-//   };
+  function onDateSelected(event, value) {
+    setDate(value);
+    setDatePicker(false);
+  };
 
-//   function onTimeSelected(event, value) {
-//     setTime(value);
-//     setTimePicker(false);
-//   };
+  function onTimeSelected(event, value) {
+    setTime(value);
+    setTimePicker(false);
+  };
 
-//   return (
-//     <SafeAreaView style={{ flex: 1 }}>
-//       <View style={styleSheet.MainContainer}>
-
-//         <Text style={styleSheet.text}>Date = {date.toDateString()}</Text>
-
-//         <Text style={styleSheet.text}>Time = {time.toLocaleTimeString('en-US')}</Text>
-
-//         {datePicker && (
-//           <DateTimePicker
-//             value={date}
-//             mode={'date'}
-//             display={Platform.OS === 'ios' ? 'spinner' : 'default'}
-//             is24Hour={true}
-//             onChange={onDateSelected}
-//             style={styleSheet.datePicker}
-//           />
-//         )}
-
-//         {timePicker && (
-//           <DateTimePicker
-//             value={time}
-//             mode={'time'}
-//             display={Platform.OS === 'ios' ? 'spinner' : 'default'}
-//             is24Hour={false}
-//             onChange={onTimeSelected}
-//             style={styleSheet.datePicker}
-//           />
-//         )}
-
-//         {!datePicker && (
-//           <View style={{ margin: 10 }}>
-//             <Button title="Show Date Picker" color="green" onPress={showDatePicker} />
-//           </View>
-//         )}
-
-//         {!timePicker && (
-//           <View style={{ margin: 10 }}>
-//             <Button title="Show Time Picker" color="green" onPress={showTimePicker} />
-//           </View>
-//         )}
-
-//       </View>
-//     </SafeAreaView>
-//   );
-// }
-
-// const styleSheet = StyleSheet.create({
-
-//   MainContainer: {
-//     flex: 1,
-//     padding: 6,
-//     alignItems: 'center',
-//     backgroundColor: 'white'
-//   },
-
-//   text: {
-//     fontSize: 25,
-//     color: 'red',
-//     padding: 3,
-//     marginBottom: 10,
-//     textAlign: 'center'
-//   },
-
-//   // Style for iOS ONLY...
-//   datePicker: {
-//     justifyContent: 'center',
-//     alignItems: 'flex-start',
-//     width: 320,
-//     height: 260,
-//     display: 'flex',
-//   },
-
-// });
-
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-const AppointmentScreen = () => {
   return (
-    <View>
-      <Text>AppointmentScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styleSheet.MainContainer}>
+
+
+        {!datePicker && (
+          <View style={{ margin: 10 }}>
+            <Button title="Show Date Picker" color="purple" onPress={showDatePicker} />
+          </View>
+        )}
+
+        {!timePicker && (
+          <View style={{ margin: 10 }}>
+            <Button title="Show Time Picker" color="purple" onPress={showTimePicker} />
+          </View>
+        )}
+
+        
+        <Text style={styleSheet.text}>Date = {date.toDateString()}</Text>
+
+        <Text style={styleSheet.text}>Time = {time.toLocaleTimeString('en-US')}</Text>
+
+        {datePicker && (
+          <DateTimePicker
+            value={date}
+            mode={'date'}
+            display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+            is24Hour={true}
+            onChange={onDateSelected}
+            style={styleSheet.datePicker}
+          />
+        )}
+
+        {timePicker && (
+          <DateTimePicker
+            value={time}
+            mode={'time'}
+            display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+            is24Hour={false}
+            onChange={onTimeSelected}
+            style={styleSheet.datePicker}
+          />
+        )}
+
+      </View>
+    </SafeAreaView>
+  );
 }
 
-export default AppointmentScreen
+const styleSheet = StyleSheet.create({
 
-const styles = StyleSheet.create({})
+  MainContainer: {
+    flex: 1,
+    padding: 6,
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+
+  text: {
+    fontSize: 25,
+    color: 'red',
+    padding: 3,
+    marginBottom: 10,
+    textAlign: 'center'
+  },
+
+  // Style for iOS ONLY...
+  datePicker: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    width: 320,
+    height: 260,
+    display: 'flex',
+  },
+
+});
