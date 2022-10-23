0d764ea66d3717c9c950fc1ad5b01b1528a5211c
Vikas Kamarapu <vikaskamarapu2001@gmail.com>
2022-10-23 07:13:11 +0530

    profile edited

--- client/screens/profileScreen.js
+++ client/screens/profileScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   StyleSheet,
   Text,
@@ -6,35 +6,34 @@ import {
   Image,
 } from 'react-native';
 
-export default class ProfileScreen extends Component {
+const ProfileScreen = () => {
 
-  render() {
-    return (
-      <View style={styles.container}>
-          <View style={styles.header}>
-            <View style={styles.headerContent}>
-                <Image style={styles.avatar}
-                  source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.headerContent}>
+          <Image style={styles.avatar}
+            source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} />
 
-                <Text style={styles.name}>Rajesh T</Text>
-                <Text style={styles.userInfo}>rajesh@gmail.com </Text>
-                <Text style={styles.userInfo}>Gachibowli </Text>
-                <Text style={styles.userInfo}>9704401708</Text>
+          <Text style={styles.name}>Rajesh T</Text>
+          <Text style={styles.userInfo}>rajesh@gmail.com </Text>
+          <Text style={styles.userInfo}>Gachibowli </Text>
+          <Text style={styles.userInfo}>9704401708</Text>
 
-            </View>
-          </View>
+        </View>
       </View>
-    );
-  }
+    </View>
+  );
+
 }
 
 const styles = StyleSheet.create({
-  header:{
+  header: {
     backgroundColor: "#DCDCDC",
-    width:390,
+    width: 390,
   },
-  headerContent:{
-    padding:30,
+  headerContent: {
+    padding: 30,
     alignItems: 'center',
   },
   avatar: {
@@ -43,46 +42,48 @@ const styles = StyleSheet.create({
     borderRadius: 63,
     borderWidth: 4,
     borderColor: "white",
-    marginBottom:10,
+    marginBottom: 10,
   },
-  name:{
-    fontSize:22,
-    color:"#000000",
-    fontWeight:'600',
-    marginBottom:4,
+  name: {
+    fontSize: 22,
+    color: "#000000",
+    fontWeight: '600',
+    marginBottom: 4,
   },
-  userInfo:{
-    fontSize:16,
-    color:"#778899",
-    fontWeight:'600',
-    marginBottom:4,
+  userInfo: {
+    fontSize: 16,
+    color: "#778899",
+    fontWeight: '600',
+    marginBottom: 4,
   },
-  body:{
+  body: {
     backgroundColor: "#778899",
-    height:500,
-    alignItems:'center',
+    height: 500,
+    alignItems: 'center',
   },
-  item:{
-    flexDirection : 'row',
+  item: {
+    flexDirection: 'row',
   },
-  infoContent:{
-    flex:1,
-    alignItems:'flex-start',
-    paddingLeft:5
+  infoContent: {
+    flex: 1,
+    alignItems: 'flex-start',
+    paddingLeft: 5
   },
-  iconContent:{
-    flex:1,
-    alignItems:'flex-end',
-    paddingRight:5,
+  iconContent: {
+    flex: 1,
+    alignItems: 'flex-end',
+    paddingRight: 5,
   },
-  icon:{
-    width:30,
-    height:30,
-    marginTop:20,
+  icon: {
+    width: 30,
+    height: 30,
+    marginTop: 20,
   },
-  info:{
-    fontSize:18,
-    marginTop:20,
+  info: {
+    fontSize: 18,
+    marginTop: 20,
     color: "#FFFFFF",
-  }
+  }
 });
+
+export default ProfileScreen;
